8f7005e0e432b0a3436d1015471456868dbe258b
邓棋栊 <dengql@hunktimes.com>
2020-09-17 10:24:14 +0800

    init

---
+++ src/main/java/com/dql/websocket/model/ChatMessage.java
@@ -0,0 +1,19 @@
+package com.dql.websocket.model;
+
+import lombok.Data;
+
+@Data
+public class ChatMessage {
+
+    private MessageType type;
+
+    private String content;
+
+    private String sender;
+
+    public enum MessageType {
+        CHAT,
+        JOIN,
+        LEAVE
+    }
+}
